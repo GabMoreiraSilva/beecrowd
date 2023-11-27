b7ea928fb42253eb8a0cbedfd3914af0bbaeb3f3
GabMoreiraSilva <g.moreirasilva1999@gmail.com>
2023-11-27 18:24:41 -0300

    exercicios de SQL

---
+++ SQL/2476.sql
@@ -0,0 +1,5 @@
+-- Autor: Gabriel Moreira Silva
+-- Submissão: 26/11/2023 23:15
+-- Problem 2746
+
+SELECT replace(name, 'H1', 'X') as name FROM virus 
---
+++ SQL/2744.sql
@@ -0,0 +1,5 @@
+-- Autor: Gabriel Moreira Silva
+-- Submissão: 26/11/2023
+-- link: https://www.beecrowd.com.br/judge/pt/problems/view/2744
+
+SELECT id, password, MD5(password) as MD5 from account
